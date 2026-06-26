19004dfd73cc1485b02c7c52a381ad73220625c1
Unknown_skittle <2011h32iscool@gmail.com>
2026-06-26 18:09:13 +0200

    Add Speed module with settings and event handlers

--- modules/speed.js
+++ modules/speed.js
@@ -1,1 +1,25 @@
+module.exports = {
+    name: "Speed",
+    category: "Movement",
 
+    settings: {
+        multiplier: {
+            type: "number",
+            default: 1.2,
+            min: 1.0,
+            max: 3.0
+        }
+    },
+
+    onEnable() {
+        chat.print("Speed enabled")
+    },
+
+    onDisable() {
+        chat.print("Speed disabled")
+    },
+
+    onTick() {
+        player.setSpeed(this.settings.multiplier)
+    }
+}
